936825f0e028235067711e98d5011482834238e4
2935580139@qq.com <2935580139@qq.com>
2025-03-13 16:37:37 +0800

    213

--- docs/.vuepress/.temp/git/config.js
+++
@@ -1,9 +0,0 @@
-import { GitContributors } from "D:/test/template/lmf04.github.io/node_modules/@vuepress/plugin-git/lib/client/components/GitContributors.js";
-import { GitChangelog } from "D:/test/template/lmf04.github.io/node_modules/@vuepress/plugin-git/lib/client/components/GitChangelog.js";
-
-export default {
-  enhance: ({ app }) => {
-    app.component("GitContributors", GitContributors);
-    app.component("GitChangelog", GitChangelog);
-  },
-};
